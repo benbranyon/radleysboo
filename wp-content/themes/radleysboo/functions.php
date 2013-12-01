b7532a91b2b0e8aba97805d7b882d5088eacb42e
Ben Branyon <ben_branyon@yahoo.com>
2013-12-01 10:48:36 -0800

    removed protected form title filter added

--- wp-content/themes/radleysboo/functions.php
+++ wp-content/themes/radleysboo/functions.php
@@ -115,4 +115,19 @@ function my_password_post_filter( $where = '' ) {
 
 add_filter( 'posts_where', 'my_password_post_filter' );
 
+function the_title_trim($title) {
+    $title = attribute_escape($title);
+    $findthese = array(
+        '#Protected:#',
+        '#Private:#'
+    );
+    $replacewith = array(
+        '', // What to replace "Protected:" with
+        '' // What to replace "Private:" with
+    );
+    $title = preg_replace($findthese, $replacewith, $title);
+    return $title;
+}
+add_filter('the_title', 'the_title_trim');
+
 
